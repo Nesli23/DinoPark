6fc1dea56e0ff4f6d28e9406d314f34c747416d0
Nesli23 <nesli.kinali@hotmail.com>
2025-02-13 09:36:36 +0100

    Corr. the xpath string to make it valid in python, added ' '

--- variables.py
+++ variables.py
@@ -7,3 +7,8 @@ current_directory = os.getcwd().replace('\\', '/')
 browser_test = "chrome" 
 url_test = f"file:///{current_directory}/webpages/jurap.html"
 title_test = "Login Test"
+
+usernme_input_id_test = "id=login-username"
+password_input_id_test = "id=login-password"
+login_button_test = 'xpath=//*[@id="login-form"]/button'
+
