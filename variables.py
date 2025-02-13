65087a166bb6cd8d8eebf1831d8b863de6df2e32
Nesli23 <nesli.kinali@hotmail.com>
2025-02-13 10:49:25 +0100

    Corr. name of the page and entered the correct xpath

--- variables.py
+++ variables.py
@@ -6,9 +6,10 @@ current_directory = os.getcwd().replace('\\', '/')
 # Demo specific
 browser_test = "chrome" 
 url_test = f"file:///{current_directory}/webpages/jurap.html"
-title_test = "Login Test"
+title_test = "Jura-Stina-Kalle Park"
 
 username_input_id_test = "id=login-username"
 password_input_id_test = "id=login-password"
-login_button_test = 'xpath=//*[@id="login-form"]/button'
+login_button_test = "xpath=//*[@id='login-nav']/a"
 
+invalid_username = "incorrectusername"
