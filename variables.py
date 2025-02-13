a01ce0275bb20361bd63ee5079335db9291f3889
Nesli23 <nesli.kinali@hotmail.com>
2025-02-13 12:22:21 +0100

    New variables.py file

---
+++ variables.py
@@ -0,0 +1,17 @@
+import os # används för att kunna referera till mappen vi är i 
+
+# Directory
+current_directory = os.getcwd().replace('\\', '/')
+
+# Invalid credentials
+invalid_username = "incorrectusername"
+invalid_password = "invalidpassword"
+
+# Demo specific
+browser_test = "chrome" 
+url_test = f"file:///{current_directory}/webpages/jurap.html"
+title_test = "Jura-Stina-Kalle Park"
+
+username_input_id_test = "id=login-username"
+password_input_id_test = "id=login-password"
+login_button_test = "xpath=//*[@id='login-nav']/a"
