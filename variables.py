b710745fdd9cc5f59d84170dc610a49afcc9fe81
Nesli23 <nesli.kinali@hotmail.com>
2025-02-14 09:42:38 +0100

    Added Submit button and messages

--- variables.py
+++ variables.py
@@ -7,6 +7,9 @@ current_directory = os.getcwd().replace('\\', '/')
 invalid_username = "incorrectusername"
 invalid_password = "invalidpassword"
 
+#Timeout
+standard_timeout = "10s"
+
 # Demo specific
 browser_test = "chrome" 
 url_test = f"file:///{current_directory}/webpages/jurap.html"
@@ -15,3 +18,14 @@ title_test = "Jura-Stina-Kalle Park"
 username_input_id_test = "id=login-username"
 password_input_id_test = "id=login-password"
 login_button_test = "xpath=//*[@id='login-nav']/a"
+submit_button_test = "xpath=//*[@id='login-form']/button"
+
+# Error Message
+verify_message_element_test = "xpath=//*[@id='login-message']"
+verify_message_test = "Verifying credentials..."
+
+error_message_element_test = "xpath=//*[@id='login-message']"
+error_message_test = "Invalid username or password"
+
+
+#"id=login-messsage"
